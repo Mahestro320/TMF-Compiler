7c6b08f6fa12e0eb9b5815bb1631d98134337d3e
Mahestro_320 <142752255+Mahestro320@users.noreply.github.com>
2024-08-16 18:57:29 +0200

    Update v1.7 - adding files

---
+++ TMF Font Compiler/src/main/java/com/main/Main.java
@@ -0,0 +1,17 @@
+package com.main;
+
+import com.scripts.TMFCompiler;
+
+public class Main {
+	/*
+	 * TMF Font Compiler v1.7, created the 08.01.24
+	 * copyright © Commys corporation.
+	 * 
+	 * For more informations, please visit
+	 * https://github.com/Mahestro320/TMF-Compiler
+	 */
+
+	public static void main(String[] args) {
+		new TMFCompiler(args);
+	}
+}
