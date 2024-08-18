8082b7a9898600bdb1fb52b1fd812409f9fc51d4
Mahestro_320 <142752255+Mahestro320@users.noreply.github.com>
2024-08-18 20:44:19 +0200

    Update v1.8 - deleting files

--- TMF Font Compiler/src/main/java/com/scripts/LineColors.java
+++
@@ -1,9 +0,0 @@
-package com.scripts;
-
-public enum LineColors {
-    // the differents types of line colors
-
-    TRANSPARENT, // when the line is transparent
-    BLACK, // when it's black
-    CUSTOM_COLOR; // the color of the font (white in the image)
-}
